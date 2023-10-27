3539a1e59bc70b69d6045cd2e1f51c4df05b7f8e
Nguyenthanh04 <tytytititity@gmail.com>
2023-10-27 16:50:01 +0700

    class name's Human

---
+++ QuanLyDanhSach_NhapVatTu/src/quanlydanhsach_nhapvattu/Human.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quanlydanhsach_nhapvattu;
+
+/**
+ *
+ * @author Administrator
+ */
+import java.util.*;
+public class Human {
+    private String name;
+    private String contact;
+
+    public void Input() {
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public void Output() {
+        // Output implementation
+        System.out.println("Name: " + name);
+        System.out.println("Contact: " + contact);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+}
